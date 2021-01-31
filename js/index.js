d3911c4033f9254c0e06d0843527052a0e2760b8
frcroth <frcroth@gmail.com>
2021-01-31 18:43:28 +0100

    Add stations with click

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ class Map {
 
     init() {
         this.map = L.map('map', {
-            center: [51.505, -0.09],
+            center: [52.511, 13.411],
             zoom: 13
         });
 
@@ -21,8 +21,51 @@ class Map {
     }
 
     onMapClick(event) {
-        console.log(event);
+        this.addStation(event.latlng)
     }
+
+    async getStationIcon() {
+        const path = "assets/stations/u-bahn.svg"
+        let station = await ajax(path);
+        return station;
+    }
+
+    get stationWidth() {
+        return 0.001;
+    }
+
+    get stationHeight() {
+        return 0.001;
+    }
+
+    bounds(position) {
+        let corner1 = L.latLng(position.lat-this.stationWidth/2, position.lng-this.stationHeight/2);
+        let corner2 = L.latLng(position.lat+this.stationWidth/2, position.lng+this.stationHeight/2);
+        return L.latLngBounds(corner1, corner2);
+    }
+
+    async addStation(position) {
+        let svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svgElement.setAttribute('xmlns', "http://www.w3.org/2000/svg");
+        svgElement.setAttribute('viewBox', "0 0 200 200");
+        let station = await this.getStationIcon();
+        svgElement.innerHTML = station;
+        let svgElementBounds = this.bounds(position);
+        L.svgOverlay(svgElement, svgElementBounds).addTo(this.map);
+    }
+}
+
+
+function ajax(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(this.responseText);
+        };
+        xhr.onerror = reject;
+        xhr.open('GET', url);
+        xhr.send();
+    });
 }
 
 document.map = new Map();
